68789814a6ba20054a5f69e7fda8597eb6038ec6
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-08-07 17:03:14 +0600

    testing

--- router/UploadRoute.js
+++ router/UploadRoute.js
@@ -9,17 +9,8 @@ router.get('/api/get', async(req,res)=>{
     res.send(allPhotots)
 });
 
-router.post("/api/save", uploadMiddleware.single("photo"), async (req, res)=>{
-    const photo = req.file.filename;
-    console.log(photo);
-    await UploadModel.create({photo})
-    .then((data)=>{
-        console.log("Uploaded successfully...");
-        console.log(data);
-        res.send(data);
-    }).catch((err)=>{
-        console.log(err)
-    })
+router.post("/api/save", async (req, res)=>{
+    res.send("successfull")
 });
 
 module.exports = router;
